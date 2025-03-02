cafb3db860f6a8134e9d0b4d5e7ff80bdda6a97d
S1nRTax <badreddine.asaad4@gmail.com>
2025-03-02 23:54:18 +0000

    added connectedUser model

---
+++ src/main/java/com/chatapp/models/connUser.java
@@ -0,0 +1,36 @@
+package com.chatapp.models;
+
+
+/**
+ * This class represents the logged-in user, so basically an instance
+ * will be passed after a successful login, that instance will have
+ * username, email, list of friends...
+ */
+
+public class connUser {
+
+
+    private String username;
+    private String email;
+
+    public connUser(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
